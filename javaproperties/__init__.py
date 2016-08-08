c8d86d40466966aa879b3e2c797ff2b70bc1255f
John T. Wodder II <git@varonathe.org>
2016-08-08 21:09:32 +0000

    "Improved" __init__.py

--- javaproperties/__init__.py
+++ javaproperties/__init__.py
@@ -1,5 +1,15 @@
-from .reading import read_properties, iter_properties, parse_properties, unescape
-
-from .writing import write_properties, join_key_value, escape
-
 from .propclass import Properties
+from .reading   import (read_properties, iter_properties, parse_properties,
+                        unescape)
+from .writing   import write_properties, join_key_value, escape
+
+__all__ = [
+    'Properties',
+    'escape',
+    'iter_properties',
+    'join_key_value',
+    'parse_properties',
+    'read_properties',
+    'unescape',
+    'write_properties',
+]
