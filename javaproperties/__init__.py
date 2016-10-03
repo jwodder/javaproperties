076f4abdd8c2e9bcb5c2fec8cf0050a1f2ecc0da
John T. Wodder II <git@varonathe.org>
2016-10-03 02:41:37 +0000

    Version 0.1.0

--- javaproperties/__init__.py
+++ javaproperties/__init__.py
@@ -14,7 +14,7 @@ from .reading   import load, loads, parse, unescape
 from .writing   import dump, dumps, join_key_value, escape, to_comment
 from .xml       import load_xml, loads_xml, dump_xml, dumps_xml
 
-__version__      = '0.1.0.dev1'
+__version__      = '0.1.0'
 __author__       = 'John Thorvald Wodder II'
 __author_email__ = 'javaproperties@varonathe.org'
 __license__      = 'MIT'
