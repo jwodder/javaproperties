b12337b9e990a68e2c9a178cdd3a7aa0bad51c2f
John T. Wodder II <git@varonathe.org>
2016-11-01 13:25:25 +0000

    Delete now-unused code

--- javaproperties/util.py
+++ javaproperties/util.py
@@ -1,9 +1,6 @@
-import codecs
 import collections
 from   decimal import Decimal
-import io
 import json
-import sys
 from   six     import string_types
 
 def strify_dict(d):
@@ -28,34 +25,3 @@ def itemize(kvs, sort_keys=False):
     if sort_keys:
         items = sorted(items)
     return items
-
-if hasattr(sys.stdin, 'buffer'):
-    propin  = codecs.getreader('iso-8859-1')(sys.stdin.buffer)
-    propout = codecs.getwriter('iso-8859-1')(sys.stdout.buffer)
-else:
-    # We're running under either Python 2 or py.test (which sets stdin to a
-    # "null" object)
-    propin  = codecs.getreader('iso-8859-1')(sys.stdin)
-    propout = codecs.getwriter('iso-8859-1')(sys.stdout)
-
-def properties_reader(fname):
-    # based on argparse.FileType
-    if fname == '-':
-        return propin
-    else:
-        try:
-            return io.open(fname, 'rt', encoding='iso-8859-1')
-        except IOError as e:
-            import argparse
-            raise argparse.ArgumentTypeError('{0}: {1}'.format(fname, e))
-
-def properties_writer(fname):
-    # based on argparse.FileType
-    if fname == '-':
-        return propout
-    else:
-        try:
-            return io.open(fname, 'wt', encoding='iso-8859-1')
-        except IOError as e:
-            import argparse
-            raise argparse.ArgumentTypeError('{0}: {1}'.format(fname, e))
