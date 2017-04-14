69055d4019ed5283102baf4241bfa0cb5fe56cb2
John T. Wodder II <git@varonathe.org>
2017-04-14 17:59:34 +0000

    Remove unused bits from docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,15 +3,11 @@ import os.path
 import sys
 
 sys.path.insert(0, os.path.abspath('..'))
-import javaproperties
 from   javaproperties import __version__
 
 project   = 'javaproperties'
 author    = 'John T. Wodder II'
-copyright = '2016 John T. Wodder II'
-
-# If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+copyright = '2016-2017 John T. Wodder II'
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -23,14 +19,12 @@ extensions = [
 autodoc_default_flags = ['members', 'undoc-members']
 # NOTE: Do not set 'inherited-members', as it will cause all of the
 # MutableMapping methods to be listed under `Properties`.
-#autodoc_member_order = 'bysource'
 
 intersphinx_mapping = {
     'https://docs.python.org/': None,
     "python2": ("https://docs.python.org/2", None),
 }
 
-templates_path = ['_templates']
 exclude_patterns = ['_build']
 source_suffix = '.rst'
 source_encoding = 'utf-8-sig'
@@ -43,36 +37,7 @@ pygments_style = 'sphinx'
 todo_include_todos = True
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 html_last_updated_fmt = '%Y %b %d'
 html_show_sourcelink = True
 html_show_sphinx = True
 html_show_copyright = True
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #'preamble': '',
-
-    # Latex figure (float) alignment
-    #'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, project + '.tex', project + ' Documentation', author, 'manual')
-]
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, project, project + ' Documentation', [author], 3)
-]
-man_show_urls = True
