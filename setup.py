79787315c19460c3c17909ef948fca921793c5ca
John T. Wodder II <git@varonathe.org>
2018-02-18 18:22:11 +0000

    Move metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -11,47 +11,9 @@ with open(join(dirname(__file__), 'javaproperties', '__init__.py')) as fp:
     else:
         raise RuntimeError('Unable to find own __version__ string')
 
-with open(join(dirname(__file__), 'README.rst')) as fp:
-    long_desc = fp.read()
-
 setup(
-    name='javaproperties',
     version=version,
-    packages=['javaproperties'],
-    license='MIT',
-    author='John Thorvald Wodder II',
-    author_email='javaproperties@varonathe.org',
-    keywords='java properties javaproperties configfile config configuration',
-    description='Read & write Java .properties files',
-    long_description=long_desc,
-    url='https://github.com/jwodder/javaproperties',
-
-    install_requires=['six>=1.4.0,<2'],
     extras_require={
         ':python_version<"2.7"': ['ordereddict'],
     },
-
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        #'Development Status :: 5 - Production/Stable',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        # Python 3.2 doesn't support \uXXXX escapes in regular expressions, so
-        # I'm not supporting it.
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries :: Java Libraries',
-        'Topic :: Utilities',
-    ],
 )
